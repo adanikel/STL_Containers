d68d6bb905f9388db7d34e85c27f23b37d8ea3bd
adanikel <59119926+adanikel@users.noreply.github.com>
2020-09-08 23:15:49 +0300

    pair methods

--- pair/Pair_imp.cpp
+++ pair/Pair_imp.cpp
@@ -7,16 +7,59 @@ struct Pair
 	ST second;
 
 	Pair(const FT first_val, const ST second_val);
+	Pair(const Pair<FT, ST>& pair_obj); // copy constructor
+	Pair(Pair<FT, ST>&& pair_obj); // move constructor
+
+	Pair<FT, ST>& operator=(const Pair<FT, ST>& pair_obj); // copy assignment
+	Pair<FT, ST>& operator=(Pair<FT, ST>&& pair_obj); // move assignment
+
+	void swap(Pair<FT, ST>& pair_obj);
 };
+
 
 int main()
 {
-	Pair<int, float> some_pair(10, 0.2);
-	std::cout << some_pair.first << " and " << some_pair.second;
+
 	return 0;
 }
 
-template <typename FT, typename ST> 
+template <typename FT, typename ST>
 Pair<FT, ST>::Pair(const FT first_val, const ST second_val)
 	: first{ first_val }, second{ second_val }
 {}
+
+template <typename FT, typename ST>
+Pair<FT, ST>::Pair(const Pair<FT, ST>& pair_obj)
+	: first{ pair_obj.first }, second{ pair_obj.second }
+{}
+
+template <typename FT, typename ST>
+Pair<FT, ST>::Pair(Pair<FT, ST>&& pair_obj)
+	: first{ std::move(pair_obj.first) }, second{ std::move(pair_obj.second) }
+{}
+
+template <typename FT, typename ST>
+Pair<FT, ST>& Pair<FT, ST>::operator=(const Pair<FT, ST>& pair_obj)
+{
+	this->first = pair_obj.first;
+	this->second = pair_obj.second;
+
+	return *this;
+}
+
+template <typename FT, typename ST>
+Pair<FT, ST>& Pair<FT, ST>::operator=(Pair<FT, ST>&& pair_obj)
+{
+	this->first = std::move(pair_obj.first);
+	this->second = std::move(pair_obj.second);
+
+	return *this;
+}
+
+template <typename FT, typename ST>
+void Pair<FT, ST>::swap(Pair<FT, ST>& pair_obj)
+{
+	std::swap(this->first, pair_obj.first);
+	std::swap(this->second, pair_obj.second);
+}
+
