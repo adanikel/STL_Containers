cdf73492b7125dc647caf357134d8692351466ad
adanikel <59119926+adanikel@users.noreply.github.com>
2020-09-08 19:43:59 +0300

    lists of pairs

--- maps/UnorderedMapIMP.cpp
+++ maps/UnorderedMapIMP.cpp
@@ -5,20 +5,23 @@ template <typename T, typename R>
 class UnorderedMapIMP
 {
 
-	std::list<R>* element_lists; // create SZ lists
+	std::list<std::pair<T, R>>* element_lists; // create SZ lists
 	unsigned int arr_sz;
-
-public:
 	unsigned int get_hashed_value(T key);
 	unsigned int get_hashed_key(unsigned int hashed_value);
 
+	R* search_within_specific_list(unsigned int list_idx, T key);
+
+
+public:
 	void insert_element(const T key, const R value);
 	void delete_element(T key);
-	R& at(T key);
-	R& operator[](T key);
+
+	void at(T key);  // todo: return ref R
+	//R& operator[](T key);
 
 
-	UnorderedMapIMP(unsigned int sz); 
+	UnorderedMapIMP(unsigned int sz);
 	~UnorderedMapIMP();
 
 };
@@ -30,12 +33,14 @@ int main()
 
 	string_map.insert_element(some_key, 10);
 
+	string_map.at(some_key);
+
 	return 0;
 }
 
 template <typename T, typename R>
-UnorderedMapIMP<T, R>::UnorderedMapIMP<T, R>(unsigned int sz)
-	: element_lists{ new std::list<T>[sz] }, arr_sz{ sz }
+UnorderedMapIMP<T, R>::UnorderedMapIMP(unsigned int sz)
+	: element_lists{ new std::list<std::pair<T, R>>[sz] }, arr_sz{ sz }
 {}
 
 template <typename T, typename R>
@@ -50,13 +55,40 @@ unsigned int UnorderedMapIMP<T, R>::get_hashed_key(unsigned int hashed_value)
 	return hashed_value % this->arr_sz;
 }
 
+template <typename T, typename R>
+R* UnorderedMapIMP<T, R>::search_within_specific_list(unsigned int list_idx, T key)
+{
+	R* value = nullptr;
+	typename std::list<std::pair<T, R>>::iterator itr = this->element_lists[list_idx].begin();
+
+
+	while (itr != this->element_lists[list_idx].end())
+	{
+		if (itr->first == key) value = &(itr->second);
+		itr++;
+	}
+
+	return value;
+}
+
+template <typename T, typename R>
+void UnorderedMapIMP<T, R>::at(T key)
+{
+	unsigned int hashed = this->get_hashed_value(key);
+	unsigned int list_idx = this->get_hashed_key(hashed);
+
+	R* value_addr = this->search_within_specific_list(list_idx, key);
+	if (value_addr) std::cout << *value_addr;
+	else std::cout << "non existing";
+
+}
 
 template <typename T, typename R>
 void UnorderedMapIMP<T, R>::insert_element(const T key, const R value)
 {
 	unsigned int hashed = this->get_hashed_value(key);
 	unsigned int list_idx = this->get_hashed_key(hashed);
-	element_lists[list_idx].push_back(value);
+	element_lists[list_idx].push_back(std::make_pair(key, value));
 }
 
 template <typename T, typename R>
