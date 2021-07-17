2ae5cd486bd1ac518687cd1f1bf2a10a0c234284
adanikel@gmail.com <adanikel@gmail.com>
2021-07-17 12:15:55 +0300

    add consts

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -3,18 +3,19 @@
 
 tempplate <typename T>
 class Stack:
+{
 	private: 
 		T* _elem_array{ nullptr };
 		unsigned int _capacity;
+		unsigned int _DEF_CAP{ 10 };
 		
 		using iterator = T*;
-		iterator _current{ nullptr };
+		iterator _current{ nullptr }; // one past last
 		iterator _begin() { return &_elem_array[0]; };
 		iterator _end() { return &_elem_array[_capacity]; };
 
 	public:
-		Stack();
-		Stack(unsigned int sz);
+		Stack(unsigned int sz = _DEF_CAP);
 		Stack(std::initializer_list<T> lst);
 		
 		Stack(const Stack<T>& stack_obj);
@@ -26,8 +27,51 @@ class Stack:
 		bool is_empty() const; { return this->_current == this->_begin(); };
 		bool is_full() const; { return this->_current == this->end(); };
 		
-		
 		void push(const T val);
 		T pop();
 
 		~Stack();
+
+}
+
+int main()
+{
+	Stack<int> a;
+	
+	return 0;
+}
+
+template <typename T> 
+Stack<T>::Stack(unsigned int sz)
+	: _capacity{ sz }, _elem_array{ new T[_capacity] }, _current{ _begin() }
+{}
+
+template <typename T>
+Stack<T>::Stack(std::initializer_list<T> lst)
+	: _capacity{ lst.size() }, _elem_array{ new T[_capacity] }, _current{ _begin() }
+{
+        for (std::initializer_list<T>::iterator itr = lst.begin(); itr != lst.end(); itr++)
+        {
+                push(*itr);
+        }
+}
+
+template <typename T>
+Stack<T>::Stack(const Stack<T>& stack_obj)
+	: _capacity{ stack_obj.capacity() }, _elem_array{ new T[_capcity] }, _current { _begin() }
+{
+	for (Stack<T>::iterator itr stack_obj.begin(); itr != stack_obj.end(); itr++)
+	{
+		push(*itr);
+		current++;
+	}
+}
+
+template <typename T>
+Stack<T>::Stack(Stack<T>&& stack_rval)
+	: _capcity{ std::move(stack_rval._capacity) }, _elem_array{ std::move(stack_rval._elem_array }, _current{ std::move(stack_rval._current) }
+{
+	stack_rval._capcity = nullptr;
+	stack_rval._elem_array = nullptr;
+	stack_rval._current = nullptr;
+}
