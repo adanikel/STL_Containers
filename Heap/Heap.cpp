08db274776827e4eb0f1865f01631e929793dae5
adanikel@gmail.com <adanikel@gmail.com>
2021-07-20 16:18:05 +0300

    assignment operators and consts

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <initializer_list>
 
 // todo
 // 1. passing array is not possible. consider passing ptr and size?
@@ -38,6 +39,12 @@ class Heap
 		unsigned int size;
 
 		Heap(bool(*sort_mtd_ptr)(T, T));
+		Heap(bool(*sort_mtd_ptr)(T, T), const std::initializer_list<T> lst);
+		Heap(const Heap& src_heap);
+		Heap(Heap&& rvalue_heap);
+
+		Heap<T>& operator=(Heap<T>& heap_obj);
+		Heap<T>& operator=(Heap<T>&& rvalue_heap);
 
 		int get_parent_position(const unsigned int idx) const; // return array idx
 		int get_right_child_position(const unsigned int idx) const; // return array idx
@@ -84,6 +91,56 @@ int main()
 		std::cout << "elem of " << idx << " is " << x.get(idx) << std::endl;	
 		++idx;
 	}
+	
+	std::cout << "copy const y, printing values" << std::endl;
+	Heap<int> y{ x };
+	idx = 0;
+        while(idx < y.size)
+        {
+                std::cout << "elem of " << idx << " is " << y.get(idx) << std::endl;
+                ++idx;
+        }
+	
+	std::cout << "intializer list z" << std::endl;
+	Heap<int> z(&is_bigger, std::initializer_list<int>{5, 8, 3, 6, 1, 9});
+	z.sort_heap();
+	idx = 0;
+        while(idx < z.size)
+        {
+                std::cout << "elem of " << idx << " is " << z.get(idx) << std::endl;
+                ++idx;
+        }
+	
+	std::cout << "initializing heap a with rval" << std::endl;	
+	Heap<int> a(Heap<int>(&is_bigger, std::initializer_list<int>{5, 8, 3, 6, 1, 9}));	
+	a.sort_heap();
+        idx = 0;
+        while(idx < a.size)
+        {
+                std::cout << "elem of " << idx << " is " << a.get(idx) << std::endl;
+                ++idx;
+        }
+	
+	std::cout << "assigning heap a with x" << std::endl;
+        a = x;
+        a.sort_heap();
+        idx = 0;
+        while(idx < a.size)
+        {
+                std::cout << "elem of " << idx << " is " << a.get(idx) << std::endl;
+                ++idx;
+        }
+
+	std::cout << "assigning heap a with rval" << std::endl;
+        a = Heap<int>(&is_bigger, std::initializer_list<int>{5, 8, 3, 6, 1, 9});
+        a.sort_heap();
+        idx = 0;
+        while(idx < a.size)
+        {
+                std::cout << "elem of " << idx << " is " << a.get(idx) << std::endl;
+                ++idx;
+        }
+
 	return 0;
 }
 
@@ -91,6 +148,56 @@ template <typename T>
 Heap<T>::Heap(bool(*sort_mtd_ptr)(T, T))
 	: _elem_arr{ new T[_DEF_SIZE] }, _capacity{ _DEF_SIZE }, size{ 0 }, _sorting_algo{ sort_mtd_ptr }
 {}
+
+template <typename T>
+Heap<T>::Heap(bool(*sort_mtd_ptr)(T, T), const std::initializer_list<T> lst)
+        : _elem_arr{ new T[static_cast<unsigned int>(lst.size())] }, _capacity{static_cast<unsigned int>(lst.size()) }, size{ 0 }, _sorting_algo{ sort_mtd_ptr }
+{
+	for(typename std::initializer_list<T>::iterator itr = lst.begin(); itr < lst.end(); itr++)
+	{
+		this->insert(*itr);
+	}
+}
+
+template <typename T>
+Heap<T>::Heap(const Heap& src_heap)
+	: _elem_arr{ new T[src_heap._capacity] }, _capacity{ src_heap._capacity }, size{ src_heap.size }, _sorting_algo{ src_heap._sorting_algo }
+{
+	std::copy(src_heap._begin(), src_heap._end(), _elem_arr);
+}
+
+template <typename T>
+Heap<T>::Heap(Heap&& rvalue_heap)
+	: _elem_arr{ std::move(rvalue_heap._elem_arr) }, _capacity{ std::move(rvalue_heap._capacity) }, size{ std::move(rvalue_heap.size) }, _sorting_algo{ std::move(rvalue_heap._sorting_algo) }
+{
+	rvalue_heap._elem_arr = nullptr;
+}
+
+template <typename T>
+Heap<T>& Heap<T>::operator=(Heap<T>& heap_obj)
+{
+	delete[] this->_elem_arr;
+	this->_capacity = heap_obj._capacity;
+	this->_elem_arr = new T[this->_capacity];
+	std::copy(heap_obj._begin(), heap_obj._end(), this->_elem_arr);
+	this->size = heap_obj.size;
+	this->_sorting_algo = heap_obj._sorting_algo;
+	
+	return *this;
+}
+
+template <typename T>
+Heap<T>& Heap<T>::operator=(Heap<T>&& rvalue_heap)
+{
+	delete[] this->_elem_arr;
+        this->_capacity = std::move(rvalue_heap._capacity);
+        this->_elem_arr = rvalue_heap._elem_arr;
+        this->size = std::move(rvalue_heap.size);
+        this->_sorting_algo = std::move(rvalue_heap._sorting_algo);
+	
+	rvalue_heap._elem_arr = nullptr;
+	return *this;
+}
 
 template <typename T>
 void Heap<T>::_reallocate()
